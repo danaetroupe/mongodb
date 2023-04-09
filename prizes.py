eadf1d8177f7771d65ebc72d719e6de781282ae9
Danae Troupe <danaetroupe@gmail.com>
2023-04-09 14:33:09 -0400

    connected to database

--- prizes.py
+++ prizes.py
@@ -1,1 +1,8 @@
-import pymongo
+import pymongo as mongo
+
+connect = mongo.MongoClient("mongodb+srv://antoniobendanae:antoniobendanae@project.lcdadjk.mongodb.net/test")
+main = connect["Main"]
+prizesdb = main["Prizes"]
+
+year = prizesdb.find_one()
+print(year)
