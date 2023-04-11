e4aa0f18275cfc6460b5b5f0968bd71db1949efe
Danae Troupe <danaetroupe@gmail.com>
2023-04-11 12:04:42 -0400

    added back responsibilities

--- prizes.py
+++ prizes.py
@@ -7,4 +7,12 @@ prizesdb = main["Prizes"]
 year = prizesdb.find_one()
 print(year)
 
-#test
+# Powerpoint - Antonio
+# Main REPL Loop - Antonio
+
+# Queries: 
+# Select all caterogies for awards - Danae 
+# Select the names of all lauterates given a certain year and category - Antonio
+# Give (a certain number) of queries involving a certain keyword - Danae
+# See if anbody won a prize multiple data - Antonio
+# Add new prizes for new year - Ben
